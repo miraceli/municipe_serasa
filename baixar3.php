ff32e643c1c80ef1a3651adb2aee4ef5c614bc36
Miraceli Bonjardim <50146432+miraceli@users.noreply.github.com>
2021-04-27 07:12:42 -0300

    Add files via upload

---
+++ baixar3.php
@@ -0,0 +1,92 @@
+﻿<?php
+	session_start();
+	include_once('conexao3.php');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Munícipes Serasa</title>
+	<head>
+	<body>
+		<?php
+
+		$hoje = date('d/m/Y');
+		$mes = date('m');
+		$ano = date('Y');
+		$nome = 'JVE - Serasa - Anotações - ';
+		$extensao = '.xls';
+		$arquivo = $nome . $mes . $ano .$extensao;
+        //$arquivo = 'BC - Serasa - Anotações - 042021.xls';
+        
+        $html = '';
+		$html .= '<p><strong>CONCESSÃO JOINVILLE - MUNÍCIPES COM ANOTAÇÕES ATIVAS NA SERASA</strong></p>';
+		$html .= '<p><strong>Atualizado em: '.$hoje.'</strong></p></br></br>';
+		$html .= '<table border="1">';		
+		$html .= '<tr>';
+		$html .= "<td style='text-align:center;vertical-align:middle'><b>Nome Munícipe</b></td>";
+		$html .= "<td style='text-align:center;vertical-align:middle'><b>Qtd. Imóveis</b></td>";
+		$html .= "<td style='text-align:center;vertical-align:middle'><b>Tarifa</b></td>";
+		$html .= "<td style='text-align:center;vertical-align:middle'><b>Acréscimo</b></td>";
+        $html .= "<td style='text-align:center;vertical-align:middle'><b>Total</b></td>";
+		$html .= '</tr>';
+		
+        $sql = "
+		SELECT Municipe.NomeMunicipe, COUNT(DISTINCT LoteCobrancaSerasa.CodImovel) AS QtdImoveis, CONCAT('R$ ',(REPLACE((SUM(Cobranca.VlrTarifa)), '.', ','))) AS Tarifa, CONCAT('R$ ',(REPLACE((SUM(Cobranca.VlrAcrescimo)), '.', ','))) AS Acrescimo, CONCAT('R$ ',(REPLACE((SUM(Cobranca.VlrTarifa + Cobranca.VlrAcrescimo)), '.', ','))) AS Total
+		FROM LoteCobrancaSerasa 
+		INNER JOIN CobrancaSerasa ON (LoteCobrancaSerasa.NrDocto = CobrancaSerasa.NrConv) 
+		INNER JOIN Cobranca ON (CobrancaSerasa.NrCobrancaOrigem = Cobranca.NrCobranca) 
+		INNER JOIN Municipe ON (LoteCobrancaSerasa.CodMunicipe = Municipe.CodMunicipe)
+		WHERE CobrancaSerasa.Situacao = 'A'
+		GROUP BY LoteCobrancaSerasa.CodMunicipe
+		ORDER BY Municipe.NomeMunicipe;
+";
+		$consulta3 = mysqli_query($conexao3 , $sql);
+		
+		while($exibirRegistros = mysqli_fetch_array($consulta3)){
+			$html .= '<tr>';
+			$html .= "<td style='text-align:left;vertical-align:middle'>".$exibirRegistros[0].'</td>';
+			$html .= "<td style='text-align:center;vertical-align:middle'>".$exibirRegistros[1].'</td>';
+			$html .= "<td style='text-align:center;vertical-align:middle'>".$exibirRegistros[2].'</td>';
+            $html .= "<td style='text-align:center;vertical-align:middle'>".$exibirRegistros[3].'</td>';
+            $html .= "<td style='text-align:center;vertical-align:middle'>".$exibirRegistros[4].'</td>';
+			$html .= '</tr>';
+			;
+		}
+
+		$sqlTotal = "
+		SELECT 'Total', SUM(QtdImoveis) AS TotalQtdImoveis, CONCAT('R$ ',(REPLACE((SUM(Tarifa)), '.', ','))) AS TotalTarifa, CONCAT('R$ ',(REPLACE((SUM(Acrescimo)), '.', ','))) AS TotalAcrescimo, CONCAT('R$ ',(REPLACE((SUM(Total)), '.', ','))) AS TotalTotal  FROM 
+		(SELECT Municipe.NomeMunicipe, COUNT(DISTINCT LoteCobrancaSerasa.CodImovel) AS QtdImoveis, SUM(Cobranca.VlrTarifa) AS Tarifa, SUM(Cobranca.VlrAcrescimo) AS Acrescimo, SUM(Cobranca.VlrTarifa + Cobranca.VlrAcrescimo) AS Total
+		FROM LoteCobrancaSerasa 
+		INNER JOIN CobrancaSerasa ON (LoteCobrancaSerasa.NrDocto = CobrancaSerasa.NrConv) 
+		INNER JOIN Cobranca ON (CobrancaSerasa.NrCobrancaOrigem = Cobranca.NrCobranca) 
+		INNER JOIN Municipe ON (LoteCobrancaSerasa.CodMunicipe = Municipe.CodMunicipe)
+		WHERE CobrancaSerasa.Situacao = 'A'
+		GROUP BY LoteCobrancaSerasa.CodMunicipe
+		ORDER BY Municipe.NomeMunicipe
+		) consulta;
+		";
+		$resultadoSqlTotal = mysqli_query($conexao3 , $sqlTotal);
+		while($exibirRegistrosSqlTotal = mysqli_fetch_array($resultadoSqlTotal)){
+			$html .= '<tr><strong>';
+			$html .= "<td style='text-align:right;vertical-align:middle;font-weight: bold'>".$exibirRegistrosSqlTotal[0].'</td>';
+			$html .= "<td style='text-align:center;vertical-align:middle;font-weight: bold'>".$exibirRegistrosSqlTotal[1].'</td>';
+			$html .= "<td style='text-align:center;vertical-align:middle;font-weight: bold'>".$exibirRegistrosSqlTotal[2].'</td>';
+            $html .= "<td style='text-align:center;vertical-align:middle;font-weight: bold'>".$exibirRegistrosSqlTotal[3].'</td>';
+            $html .= "<td style='text-align:center;vertical-align:middle;font-weight: bold'>".$exibirRegistrosSqlTotal[4].'</td>';
+			$html .= '</strong></tr>';
+			;
+		}
+
+		header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+		header ("Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT");
+		header ("Cache-Control: no-cache, must-revalidate");
+		header ("Pragma: no-cache");
+		header ("Content-type: application/x-msexcel");
+		header ("Content-Disposition: attachment; filename=\"{$arquivo}\"" );
+		header ("Content-Description: PHP Generated Data" );
+
+		echo $html;
+		exit; ?>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <meta charset="urf-8" />
+  <title>Munícipes Serasa</title>
+  <link rel="stylesheet" href="_css/style_consulta.css" />
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="bootstrap/js/bootstrap.js" rel="stylesheet">
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+    <a class="navbar-brand" href=planilhas.php>Munícipes Serasa</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+  </nav>
+
+  <div class="container">
+
+    <section>
+      <br><br><br>
+      <h3>Balneário Camboriú</h3><br>
+      <a class="btn btn-primary" href=baixar1.php role="button">Baixar Excel</a>
+      <br><br><hr>
+    </section>
+
+    <section>
+      <br><br>
+      <h3>Itajaí</h3><br>
+      <a class="btn btn-primary" href=baixar2.php role="button">Baixar Excel</a>
+      <br><br><hr>
+    </section>
+
+    <section>
+      <br><br>
+      <h3>Joinville</h3><br>
+      <a class="btn btn-primary" href=baixar3.php role="button">Baixar Excel</a>
+      <br><br><hr>
+    </section>
+
+    <section>
+      <br><br>
+      <h3>São José</h3><br>
+      <a class="btn btn-primary" href=baixar4.php role="button">Baixar Excel</a>
+      <br><br><hr>
+    </section>
+
+    <section>
+      <br><br>
+      <h3>Itapema</h3><br>
+      <a class="btn btn-primary" href=baixar5.php role="button">Baixar Excel</a>
+      <br><br><hr>
+    </section>
+
+    <section>
+      <br><br>
+      <h3>São Francisco do Sul</h3><br>
+      <a class="btn btn-primary" href=baixar6.php role="button">Baixar Excel</a>
+      <br><br><hr>
+    </section>
+
+  </div>
+</body>
+
+</html>
